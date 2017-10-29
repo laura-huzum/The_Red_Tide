775a56f79a96af1a65b2141042497dc30d3e0907
laura-huzum <33130610+laura-huzum>
2017-10-29 14:52:05 +0200

    plasare pusca faza1
    momentan machine gunu urmareste mouseu si se pune acolo unde dai click

---
+++ Assets/Scripts/MachineGunBehaviour.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MachineGunBehaviour : MonoBehaviour
+{
+
+    GameObject structure_prefab;
+    GameObject structure;
+    bool shooting_state;
+
+    // Use this for initialization
+    void Start()
+    {
+
+
+
+    }
+
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        if (!shooting_state)
+        {
+            gameObject.transform.position = Vector2.Lerp(transform.position, Camera.main.ScreenToWorldPoint(Input.mousePosition), 1);
+        }
+        else
+        {
+            // shooting at stuff
+        }
+
+    }
+
+
+    public void PlaceAction()
+    {
+
+        structure = (GameObject)Instantiate(structure_prefab, Camera.main.ScreenToViewportPoint(Input.mousePosition), Quaternion.identity);
+        shooting_state = false;
+
+    }
+
+    void OnMouseDown()
+    {
+
+        if(!shooting_state)
+        {
+            // object is being placed
+            shooting_state = true;
+        }
+
+
+    }
+}
