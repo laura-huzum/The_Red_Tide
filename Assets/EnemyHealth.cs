02e1fa941f296466d5969e45c820c168cf577dfc
teoxyz95 <teoxyz95@users.noreply.github.com>
2017-10-31 20:29:34 +0200

    Spawn and tag

---
+++ Assets/EnemyHealth.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour {
+
+    private int health = 5;
+
+    void Damage()
+    {
+        health--;
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (health == 0)
+            return;
+	}
+}
